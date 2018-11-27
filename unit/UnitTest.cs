1c6aee99b7df93e0de6abaa72dd9ed37937c906c
salihkemaloglu <salihkemaloglu@gmail.com>
2018-11-27 11:33:50 +0300

    add gate for docker check unit test

--- unit/UnitTest.cs
+++ unit/UnitTest.cs
@@ -1,5 +1,7 @@
 using System;
+using System.IO;
 using System.Linq;
+using System.Xml.Serialization;
 using webapi.Controllers;
 using Xunit;
 
@@ -10,18 +12,51 @@ namespace unitTest
         [Fact]
         public void Test()
         {
-            var controller=new ValuesController();
-           
-            var result=controller.Get();     
-            Assert.Equal(2,result.Count());
+            var controller = new ValuesController();
+            var result = controller.Get();
+            Assert.Equal(2, result.Count());
         }
         [Fact]
         public void Test1()
         {
-            var controller=new ValuesController();
+            var controller = new ValuesController();
 
-            var result2=controller.Get(1);
-            Assert.Equal("val",result2.ToString());
+            var result2 = controller.Get(1);
+            Assert.Equal("value", result2.ToString());
         }
+
+        [Fact]
+        public void Test2()
+        {
+            var controller = new ValuesController();
+
+            var result2 = controller.Get(1);
+            Assert.Equal("value", result2.ToString());
+        }
+        [Fact]
+        public void Test3()
+        {
+            var controller = new ValuesController();
+
+            var result2 = controller.Get(1);
+            Assert.Equal("value", result2.ToString());
+        }
+        [Fact]
+        public void Test4()
+        {
+            var controller = new ValuesController();
+
+            var result2 = controller.Get(1);
+            Assert.Equal("value", result2.ToString());
+        }
+        [Fact]
+        public void Test5()
+        {
+            var controller = new ValuesController();
+
+            var result2 = controller.Get(1);
+            Assert.Equal("value", result2.ToString());
+        }
+
     }
 }
